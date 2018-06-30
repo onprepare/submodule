3870269e9fb200def8d6d80012445a4aec5d5e73
qiangliu <liuq_jxnanc@163.com>
2018-06-30 12:32:20 +0800

    First git submodule

---
+++ aaa.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	int i=0;
+	int j=0;
+
+	printf("aaaaaaaaaaaaaa!\n");
+	printf("bbbbbbbbbbbbbb!\n");
+	
+	for(i=0; i<10; i++)
+	{
+		j++;
+		printf("qiangliu fdfsfsd:%d\n", j);
+	}
+	
+	return 0;
+}
